f31be02d243a9a1129b7753ac2508cc56eb931f8
djuta <devenjuta@gmail.com>
2014-04-28 00:52:12 -0400

    first release

--- filtergif/settings.py
+++ filtergif/settings.py
@@ -1,7 +1,8 @@
 CONFIG = {
-    'client_id':'6b8b8320cc194b50ad1bff2a8026d167',
-    'client_secret':'2c226bb025254c838ea9f2b4bb13c6e5',
-    'redirect_uri':'http://djuta.webfactional.com/filtergif/oauth_callback'
+    'client_id':'',
+    'client_secret':'',
+    'redirect_uri':''
 }
 
-GIF_PATH = '/home/djuta/webapps/static/gifs/'
+# path to where you want the gifs saved
+GIF_PATH = ''
